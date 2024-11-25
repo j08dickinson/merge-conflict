e70dbcb69713294e9d9ab91d12235e04cbc4dc3b
Charles Palmer <charlesaltonpalmer@gmail.com>
2024-11-25 09:06:46 -0700

    Simplified score range calculator

--- score_range_calculator.py
+++ score_range_calculator.py
@@ -1,16 +1,5 @@
 def calculate_score_range(scores):
-  score_range = 0
-  max_score = 0
-  min_score = 100
-
-  for score in scores:
-    if int(score) > max_score:
-      max_score = int(score)
-    if int(score) < min_score:
-      min_score = int(score)
-
-  score_range = max_score - min_score
-  return score_range
+  return max(scores) - min(scores)
 
 # ------------------------------------------------------
 if __name__ == '__main__':
